11081f905ce461ef07b4ddf48973a3c77f2a1cbb
jimin <jm131313.kim@gmail.com>
2023-03-08 22:10:03 +0900

    [fix] css 수정

--- components/Layout/DefaultLayout.tsx
+++ components/Layout/DefaultLayout.tsx
@@ -28,7 +28,7 @@ const DefaultLayout = ({ children, isShowMenu = true, backgroundColor, height }:
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 100%;
+  // height: 100%;
   margin: auto 0;
   background: #f5f5f5;
 `;
