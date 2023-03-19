884c19f2f93e07d96b0fd4a2eb065d77cf1ca112
jimin <jm131313.kim@gmail.com>
2023-03-19 17:11:55 +0900

    [feat] rate limiter 적용

--- apis/instance.ts
+++ apis/instance.ts
@@ -1,8 +1,21 @@
 import axios, { AxiosError, AxiosInstance } from 'axios';
+import axiosRetry from 'axios-retry';
 import { checkToken } from '@/utils/serviceUtils';
 
 const Instance: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_CODOG_BACK_URL,
+});
+
+// Rate Limiter
+axiosRetry(Instance, {
+  retries: 5,
+  retryDelay: (retry) => {
+    const delay = Math.pow(2, retry) * 100;
+    const jitter = delay * 0.1 * Math.random();
+    return delay + jitter;
+  },
+  retryCondition: (err) =>
+    axiosRetry.isNetworkOrIdempotentRequestError(err) || err.response?.status === 429,
 });
 
 Instance.interceptors.request.use(async (config) => {
