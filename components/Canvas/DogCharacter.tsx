dd386a59592a01e3cc1d7bc9c1f6aaf4a45c2535
jimin <jm131313.kim@gmail.com>
2023-04-25 14:44:59 +0900

    [feat] Dog Character 에니메이션 추가

--- components/Canvas/DogCharacter.tsx
+++ components/Canvas/DogCharacter.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
 import { CHARACTER_CODES } from '@/apis/type';
 
 interface DogCharacterProps {
@@ -9,15 +11,20 @@ interface DogCharacterProps {
   left?: string;
 }
 
-// const getInfo = (code: string) => {
-//   const defaultValue = { bottom: '-0.5rem' };
-//   switch (code) {
-//     // case 'C':
-//     //   return { ...defaultValue, bottom: '0' };
-//     default:
-//       return defaultValue;
-//   }
-// };
+const getInfo = (code: string) => {
+  const defaultValue = { width: '103px', height: '146px' };
+  switch (code) {
+    case 'A':
+      return { ...defaultValue, width: '103px', height: '146px' };
+    case 'B':
+      return { ...defaultValue, width: '94px', height: '129px' };
+    case 'C':
+      return { ...defaultValue, width: '111px', height: '128px' };
+    default:
+      return defaultValue;
+  }
+};
+
 const DogCharacter = ({
   character,
   pose = 'Default',
@@ -26,22 +33,46 @@ const DogCharacter = ({
   left,
 }: DogCharacterProps) => {
   const dogImage = `/images/dogs/${character}/${clothes}_${pose}.svg`;
+  const { height, width } = getInfo(character);
+  function AnimatedDogCharacter({ img }: { img: string }) {
+    return <DogCharacterDiv img={img} height={height} width={width} onClick={onClick} />;
+  }
   // TODO: 노트북은 커밋 갯수에따라 자동 업그레이드 되어야 한다.
   return (
     <>
-      <DogCharacterDiv img={dogImage} onClick={onClick} />
+      <AnimatedDogCharacter img={dogImage} />
       <Laptop left={left} />
     </>
   );
 };
 
-const DogCharacterDiv = styled.div<{ img: string; bottom?: string }>`
-  content: url(${(props) => `${props.img ? props.img : '/images/dogs/A/Default_Default.svg'}`});
+const slideIn = keyframes`
+  0% {
+    transform: translate(-50%, -10%);
+  }
+  100% {
+    transform: translate(-50%, 0);
+  }
+`;
+
+const DogCharacterDiv = styled.div<{
+  img: string;
+  bottom?: string;
+  height?: string;
+  width?: string;
+}>`
+  background-image: url(${(props) =>
+    `${props.img ? props.img : '/images/dogs/A/Default_Default.svg'}`});
   position: absolute;
   left: 53%;
   transform: translate(-50%, 0%);
+  animation-name: ${slideIn};
+  animation-duration: 0.1s;
+  animation-timing-function: ease-out;
   bottom: -0.5rem;
   cursor: pointer;
+  width: ${(props) => `${props.width ? props.width : '100px'}`};
+  height: ${(props) => `${props.height ? props.height : '100px'}`}; ;
 `;
 
 const Laptop = styled.div<{ left?: string }>`
@@ -55,4 +86,4 @@ const Laptop = styled.div<{ left?: string }>`
   }
 `;
 
-export default DogCharacter;
+export default React.memo(DogCharacter);
