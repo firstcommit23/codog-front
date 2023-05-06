16e1cd02eb3eef091e362938c751c6a304ac80cb
jimin <jm131313.kim@gmail.com>
2023-05-06 22:02:36 +0900

    [chore] ga 추적태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,19 @@ export default function Document() {
           rel="stylesheet"
           as="style"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css"></link>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-L7N1RL3CXK`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-L7N1RL3CXK', {
+                  page_path: window.location.pathname,
+                });
+              `,
+          }}
+        />
       </Head>
       <body>
         <Main />
